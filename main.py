683b6c05601c9228d39f058e2108896992750e50
Martin Gamauf <martin.gamauf00@gmail.com>
2023-11-26 19:31:16 +0100

    changed decorator to run after function execution

--- main.py
+++ main.py
@@ -13,22 +13,33 @@ Created on Sun Nov 26 16:19:44 2023
 
 class Stack:
     
+    # DECORATORS #
     def update(func):
         def update_length(self, *args):
+            output = func(self, *args)
             self.length = len(self.stack)
             print("Length updated")
-            return func(self, *args)
+            return output
             
         return update_length
     
-
+    # INIT #
     def __init__(self):
         self.stack = []
         self.length = len(self.stack)
 
+    # FUNCTIONS #
     @update        
     def add(self, number: int):
+        
         self.stack.append(number)
+    
+    @update
+    def take(self, num_to_take: int):
+        ''' take the amount of num_to_take cards from the stack
+        if num_to_take < num_takable: returns the list of cards that were took
+        if num_to_take > num_takable: returns empty list '''
+        return 
 
 class Board:
     
