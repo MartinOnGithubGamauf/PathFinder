d157b80dc3ada26cbf989418c1778ffaff4dad35
Martin Gamauf <martin.gamauf00@gmail.com>
2023-12-22 00:06:06 +0100

    Working version of IDA, but still dubious execution and bad print statements

--- idastar.py
+++ idastar.py
@@ -40,7 +40,7 @@ class Edge:
     def __init__(self, destination_node, weight: int):
         self.id = 0
         self.destination = destination_node
-        self.weight = weight
+        self.weight = weight # acts as g value for the successor node
     
         self.id = Edge.COUNTID
         Edge.COUNTID = Edge.COUNTID + 1
@@ -84,9 +84,16 @@ class Graph:
     
     # INIT #
     def __init__(self):
+        # DFS
         self.node_list = []
-        
         self.root = 0 # id of node to start with
+        
+        # IDA
+        self.target = 0 # id of node to end with
+        self.stack = [] # represents the search path
+        self.path = []
+        
+        
         
         
     # MAGIC METHODS #
@@ -148,11 +155,84 @@ class Graph:
         
                     
     def solve_ida(self):
-        pass
-                
+        ''' solve with pseudocode from https://en.wikipedia.org/wiki/Iterative_deepening_A* '''
         
-if __name__=='__main__':
+        step = 0
+        found = False
+        INF = 10000
+        FOUND = -999999
+        
+        root = self.node_list[self.root]
+        target = self.node_list[self.target]
+        
+        print(f"\nStep {step}.\n")
+        print("Setting bound to h(root) and putting root in the stack.")
+        bound = root.h
+        self.stack.append(root)
+        
+        def search(stack, g, bound):
+            nonlocal found, target, INF, FOUND
+            print(f"Current: g={g}, bound={bound}.")
+            
+            node = stack[len(stack)-1] # last node in stack
+            node.f = g + node.h
+            print(f"Investigating {node}, it has f-value of {node.f}.")
+            if node.f > bound: 
+                print("The Node has a higher f-value then bound -> returning.")
+                return node.f
+            if node == target: 
+                print("We found the Target Node.")
+                return FOUND
+            print("The Node has a lower f-value then bound -> setting minn to INF")
+            minn = INF
+            
+            # order the edge_list based on the lowest destination f-values
+            node.edge_list.sort(key=lambda e: e.destination.f)
+            
+            # choose next successor with lowest f-value
+            for edge in node.edge_list:
+                
+                successor = edge.destination
+                print(f"Looking for the next successor with lowest f-value, it is {successor} with {successor.f}.")
+                
+                if successor not in stack:
+                    print("The successor was not in the stack, adding it now.\n")
+                    stack.append(successor)
+                    print("Setting the next t-value to the result of this successor:")
+                    t = search(stack, g + edge.weight, bound)
+                    if t == FOUND:
+                        print("We found the Target Node (with t).")
+                        return FOUND
+                    if t < minn:
+                        print(f"t-value is smaller than minn -> setting minn to t={t}.")
+                        minn = t
+                    print(f"Removing {stack[len(stack)-1]} the fist item from the stack.")
+                    stack.pop()
+                
+            return minn
     
+        
+        print("Entering loop")
+        while not found:
+            
+            t = search(self.stack, 0, bound)
+            if t == FOUND:
+                found = True
+                break
+            if t == INF:
+                print("t NOT FOUND")
+                break
+            bound = t
+            
+            
+        
+        
+        
+        
+        
+                
+def example_dfs():
+    ''' search through a graph with recursive DFS, from https://en.wikipedia.org/wiki/Depth-first_search'''
     g = Graph()
     
     n0 = Node(name="A", h=0)
@@ -185,6 +265,49 @@ if __name__=='__main__':
     
     g.solve_dfs()
     
+    return g
+
+def example_ida():
+    ''' IDA '''
     
+    g = Graph()
+    
+    n0 = Node(name="Saarbrücken", h=222)
+    n1 = Node(name="Kaiserslautern", h=158)
+    n2 = Node(name="Frankfurt", h=96)
+    n3 = Node(name="Ludwigshafen", h=108)
+    n4 = Node(name="Karlsruhe", h=140)
+    n5 = Node(name="Würzburg", h=0)
+    n6 = Node(name="Heilbronn", h=87)
+    
+    g.add_node(n0)
+    g.add_node(n1)
+    g.add_node(n2)
+    g.add_node(n3)
+    g.add_node(n4)
+    g.add_node(n5)
+    g.add_node(n6)
+    
+    g.add_edge(n0, n1, 70)
+    g.add_edge(n0, n4, 145)
+    g.add_edge(n1, n2, 103)
+    g.add_edge(n1, n3, 53)
+    g.add_edge(n2, n5, 116)
+    g.add_edge(n3, n5, 183)
+    g.add_edge(n4, n6, 84)
+    g.add_edge(n5, n5, 102)
+    
+    print(g)
+    
+    g.root = 0 # Saarbrücken
+    g.target = 5 # Würzburg
+    
+    g.solve_ida()
+    
+    return g
+
+if __name__=='__main__':
+    
+    g = example_ida()
     
     
